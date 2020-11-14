e0f07494995c5829a6f517b4779a1e7dec5da007
zmm <792646962@qq.com>
2020-11-14 17:26:36 +0800

    学生类

---
+++ Student.java
@@ -0,0 +1,16 @@
+package bean;
+
+/**
+ * @ProjectName: qwer
+ * @Package: bean
+ * @Description: java类作用描述
+ * @Author: 周铭明
+ * @CreateDate: 2020/11/14 0014 17:17
+ * @Version: 1.0
+ * <p>
+ * Copyright: Copyright (c) 2020
+ */
+public class Student {
+    private String name;
+    private int age;
+}
